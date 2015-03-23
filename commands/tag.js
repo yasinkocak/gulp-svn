1a317f56cf63ea6669fe802a1259de2c3d40ccb6
Yasin Kocak <y.kocak@xyn.nl>
2015-03-23 10:19:36 +0100

    Fixed svn tags

--- commands/tag.js
+++ commands/tag.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var exec = require('child_process').exec,
+    gutil = require('gulp-util'),
     svnProjectRoot = require('svn-project-root');
 
 module.exports = function(tag, message, options, cb) {
@@ -26,11 +27,13 @@ module.exports = function(tag, message, options, cb) {
             return cb(err);
         }
     }
+    var projectRoot = projectRoot.replace(/\/$/, '');
 
-    var from = projectRoot = projectRoot.replace(/\/$/, '');
+    var from = projectRoot + '/trunk/';
     var to = projectRoot + '/tags/' + tag;
 
     var cmd = 'svn cp ' + from + ' ' + to + ' -m "' + message + '" ' + options.args;
+    console.log(cmd);
 
     if(options.username && options.password) {
         cmd += ' --username '+ options.username + ' --password ' + options.password;
