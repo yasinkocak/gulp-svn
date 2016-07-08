13ad669eb7d600868266297aeeebac6ff9a3dfbc
ivinwu(吴泽明) <ivinwu@tencent.com>
2016-07-08 15:46:42 +0800

    add sync

---
+++ commands/deleteSync.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var exec = require('child_process').execSync,
+    gutil = require('gulp-util');
+
+module.exports = function (file, options, cb) {
+
+    if(!cb && typeof options === 'function') {
+        cb = options;
+        options = {};
+    }
+
+    if(!cb || typeof cb !== 'function') cb = function() {};
+    if(!options) options = {};
+    if(!file) throw new Error('gulp-svn: File is required svn.add("file.js")');
+    if(!options.cwd) options.cwd = process.cwd();
+    if(!options.args) options.args = ' ';
+
+    var cmd = 'svn delete ' + file + ' ' + options.args;
+
+    if(options.username && options.password) {
+        cmd += ' --username '+ options.username + ' --password ' + options.password;
+    }
+
+    return exec(cmd, {cwd: options.cwd});
+};
---
+++ commands/moveSync.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var exec = require('child_process').execSync,
+    gutil = require('gulp-util');
+
+module.exports = function (src, dest, options, cb) {
+
+    if(!cb && typeof options === 'function') {
+        cb = options;
+        options = {};
+    }
+
+    if(!cb || typeof cb !== 'function') cb = function() {};
+    if(!options) options = {};
+    if(!src) throw new Error('gulp-svn: Source is required');
+    if(!dest) throw new Error('gulp-svn: Destination is required');
+    if(!options.cwd) options.cwd = process.cwd();
+    if(!options.args) options.args = ' ';
+
+    var cmd = 'svn log ' + src + ' ' + dest + ' ' + options.args;
+
+    if(options.username && options.password) {
+        cmd += ' --username '+ options.username + ' --password ' + options.password;
+    }
+
+    return exec(cmd, {cwd: options.cwd});
+};
